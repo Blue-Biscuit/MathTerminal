20168865988cc3773d4ef1d821b6eed4c206d174
Blue-Biscuit <ahuffman9000@gmail.com>
2022-03-19 19:05:11 -0400

    Update MathTerminal.h

--- mterminal/MathTerminal.h
+++ mterminal/MathTerminal.h
@@ -5,6 +5,11 @@
 
 #pragma once
 
+// Global module.
 #include "globals/Definitions.h"
+#include "globals/ISimplifiable.h"
+
+// Solution module.
 #include "solution/SolutionType.h"
+#include "solution/ISolvable.h"
 #include "solution/Solution.h"
